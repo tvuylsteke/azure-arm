70eadb1d9204fa5ec4fc7ee6890b10e3d0741fb8
tvuylsteke <thovuy@microsoft.com>
2017-12-02 22:57:33 +0100

    added 3th server

--- FullSolution/configureRouter.sh
+++ FullSolution/configureRouter.sh
@@ -1,7 +1,9 @@
 #{"fileUris":["https://mystorage.blob.core.windows.net/scripts/install_lamp.sh"], "commandToExecute":"sh install_lamp.sh" }
 
-
+sudo -i
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+
 iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
 route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.97
