3669f08536188ae71d6fb3cf430f2b4047ee9874
tvuylsteke <thovuy@microsoft.com>
2017-12-18 17:57:14 +0100

    added img + fixed sh script.

--- FullSolution/configureRouter.sh
+++ FullSolution/configureRouter.sh
@@ -4,8 +4,12 @@ sudo -i
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 
-iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
-route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.97
-route add -net 10.0.3.0 netmask 255.255.255.0 gw 10.0.0.65
-reboot
+# not necessary
+#iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+#iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+
+#ensure traffic comes from the same IP. Not needed for single interface NVA's
+route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.4.1
+route add -net 10.0.2.0 netmask 255.255.255.0 gw 10.0.3.1
+
+#reboot
